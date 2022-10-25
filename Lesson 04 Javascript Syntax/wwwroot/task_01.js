788aa640f2d4801e0346b52822590e8facdd3357
bulhard <pravoslav.milenkov@gmail.com>
2022-10-25 19:15:06 +0300

    task_01

---
+++ Lesson 04 Javascript Syntax/wwwroot/task_01.js
@@ -0,0 +1,12 @@
+﻿function Calculate() {
+    let a = document.getElementById("numberA").value;
+    let b = document.getElementById("numberB").value;
+
+    console.log(a + b);
+    console.log(a - b);
+    console.log(Number(a) + Number(b));
+
+    document.getElementById("numberC").value = Number(a) + Number(b);
+
+    document.getElementById("div1").innerHTML += "<hr />" + a + '+' + b + '=' + (Number(a) + Number(b));
+}
