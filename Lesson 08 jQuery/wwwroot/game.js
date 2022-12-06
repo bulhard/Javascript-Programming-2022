afc8f9b9bfb233a8ba8cbb180811f1419d3c4963
bulhard <pravoslav.milenkov@gmail.com>
2022-12-06 23:56:56 +0200

    jquery

---
+++ Lesson 08 jQuery/wwwroot/game.js
@@ -0,0 +1,16 @@
+﻿var numberOfPlayers = 0;
+var fieldSize = 0;
+
+function StartGame() {
+
+    numberOfPlayers = Number($("#players").val());
+    fieldSize = Number($("#field_size").val());
+
+    $("#mainMenu").hide();
+    $("#gameField").show();
+
+    if (fieldSize == 3) {
+        $(".row4").hide();
+    }
+
+}
